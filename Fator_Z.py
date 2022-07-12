0b7b15c0856e47bcc55cfc48841465947f36c772
Francisco Araújo <franciscoaraujo2016f2@outlook.com>
2022-07-12 20:20:38 -0300

    initial commit

--- Fator_Z.py
+++ Fator_Z.py
@@ -1,148 +1,228 @@
-from Correlacoes_Black_Oil import *
+from Fator_Z_Correlacoes_Black_Oil import *
+Dado = input('DEFINA A CORRELAÇÃO: DIGITE "BB" PARA BRILL E BEGGS, "PP" PARA PAPAY, '
+             '"HY" PARA HALL YARBOROUGH, "DK" PARA DRANCHUK & ABU-KASSEM>> ').upper()
+dg = float(input('SE A GRAVIDADE ESPECÍFICA (DG) FOR CONHECIDA INFORME SEU VALOR! CASO CONTRÁRIO, DIGITE "0">> '))
 
-dg = float(input('SE A GRAVIDADE ESPECÍFICA (DG) FOR CONHECIDA INFORME SEU VALOR! CASO CONTRÁRIO, DIGITE "0":  '))
+if dg != 0 and Dado == "HY":
+    if dg < 0.75:  # gás seco
+        Ppc = (677 + 15 * dg - 37.5 * dg ** 2) * 6894.76  # Pa
+        Tpc = (168 + 325 * dg - 12.5 * dg ** 2) * 0.5556  # K
+    else:
+        Ppc = (706 - 51.7 * dg - 11.1 * dg ** 2) * 6894.76  # gás úmido
+        Tpc = (187 + 330 * dg - 71.5 * dg ** 2) * 0.5556
+else:
+    if dg < 0.75:  # gás seco
+        Ppc = (677 + 15 * dg - 37.5 * dg ** 2)  # Psia
+        Tpc = (168 + 325 * dg - 12.5 * dg ** 2)  # Rankine
+    else:
+        Ppc = (706 - 51.7 * dg - 11.1 * dg ** 2)  # gás úmido
+        Tpc = (187 + 330 * dg - 71.5 * dg ** 2)
+
 if dg == 0:
-    caminho = input('POSSUI DADOS DE PRESSÃO CRÍTICA E TEMPERATURA CRÍTICA? S/N: ').upper()
+    caminho = input('POSSUI DADOS DE PRESSÃO CRÍTICA E TEMPERATURA CRÍTICA? S/N>> ').upper()
     if caminho == "N":
-        print(' ')
-        print('ENCONTRANDO O DG POR FRAÇÃO MOLAR...')
-        print(' ')
-        Qnt = int(input('INFORME A QUANTIDADE DE COMPONENTES:  '))
-        unidade = input('UNIDADE DA MASSA MOLAR (MM): "kg/mol" ou "lb/mol": ').upper()
+        Qnt = int(input('INFORME A QUANTIDADE DE COMPONENTES>> '))
+        unidade = input('UNIDADE DA MASSA MOLAR (MM): "kg/mol" ou "lb/mol">> ').upper()
         MMT = 0
         YI1 = 0
         YI2 = 0
         # Tratar o erro das frações molares
         for i in range(0, Qnt):
             if unidade == "LB/MOL":
-                MM = float(input(f'INFORME A MASSA MOLAR DO {i + 1}° COMPONENTE:  '))
-                yi = float(input(f'INFORME A FRAÇÃO MOLAR DO {i + 1}° COMPONENTE:  '))
+                MM = float(input(f'INFORME A MASSA MOLAR DO {i + 1}° COMPONENTE>> '))
+                yi = float(input(f'INFORME A FRAÇÃO MOLAR DO {i + 1}° COMPONENTE>> '))
                 YI1 += yi
             elif unidade == "KG/MOL":
-                MM = float(input(f'INFORME A MASSA MOLAR DO {i + 1}° COMPONENTE:  ')) * 2.20462
-                yi = float(input(f'INFORME A FRAÇÃO MOLAR DO {i + 1}° COMPONENTE:  '))
+                MM = float(input(f'INFORME A MASSA MOLAR DO {i + 1}° COMPONENTE>> ')) * 2.20462
+                yi = float(input(f'INFORME A FRAÇÃO MOLAR DO {i + 1}° COMPONENTE>> '))
                 YI2 += yi
             MMT += yi * MM
-        print(f'yi1: {YI1}, yi2: {YI2} ')
         if YI1 != 1 or YI2 != 1:
-            raise ValueError('A soma da fração molar é diferente de 1.')
+            raise ValueError('A SOMA DA FRAÇÃO MOLAR É DIFERENTE DE 1!!!')
         dg = MMT / 28.96  # 28.96 é a massa molecular do ar. Precismos corrigir esssa
     else:
-        print(' ')
-        print('ENCONTRANDO O DG POR PRESSÕES CRÍTICAS E TEMPERATURAS CRÍTICAS...')
-        print(' ')
-        yi = input('INSIRA AS FRAÇÕES MOLARES Y1 Y2 Y3... : ').split(' ')
+        yi = input('INSIRA AS FRAÇÕES MOLARES SEPARADAS POR ESPAÇO>> ').split(' ')
         yi = [float(dado) for dado in yi]
 
-        tci = input('INSIRA AS TEMPERATURAS CRÍTICAS T1 T2 T3... EM RANKINE: ').split(' ')
-        tci = [float(dado) for dado in tci]
+        TC = input('INSIRA AS TEMPERATURAS CRÍTICAS SEPARADAS POR ESPAÇO>> ').split(' ')
+        if Dado == "HY":
+            unit_Tc = input('INFORME A UNIDADE (K, C, R OU F)>> ')
+            if unit_Tc.upper() == "C":
+                unit_Tc = [float(dados)+274.15 for dados in TC]
+            elif unit_Tc.upper() == "R":
+                unit_Tc = [float(dados) * 0.5556 for dados in TC]
+            elif unit_Tc.upper() == "F":
+                unit_Tc = [(float(dados) - 32) * 5/9 + 273.15 for dados in TC]
+            elif unit_Tc.upper() == "K":
+                unit_Tc = [float(dados) * 1 for dados in TC]
+                # CONVERSÕES PARA K
+        else:
+            unit_Tc = input('INFORME A UNIDADE (K, C, R OU F)>> ')
+            if unit_Tc.upper() == "C":
+                unit_Tc = [float(dados) + 491.67 for dados in TC]
+            elif unit_Tc.upper() == "R":
+                unit_Tc = [float(dados) for dados in TC]
+            elif unit_Tc.upper() == "F":
+                unit_Tc = [float(dados) + 459.67 for dados in TC]
+            elif unit_Tc.upper() == "K":
+                unit_Tc = [float(dados) * 1.8 for dados in TC]
+        # CONVERSÕES PARA RANKINE
+        PC = input('INSIRA AS PRESSÕES CRÍTICAS P1 P2 P3>> ').split(' ')
+        unit_Pc = input('INFORME A UNIDADE (Bar Pa Atm Torr mmHg Kgf/cm² Kgf/in² ou Psi)>> ')
+        if Dado == "HY":
+            if unit_Pc.upper() == "BAR":
+                unit_Pc = [float(dados) * 100_000 for dados in PC]
+            elif unit_Pc.upper() == "PA":
+                unit_Pc = [float(dados) for dados in PC]
+            elif unit_Pc.upper() == "ATM":
+                unit_Pc = [float(dados) * 101_325 for dados in PC]
+            elif unit_Pc.upper() == "TORR":
+                unit_Pc = [float(dados) * 133.322 for dados in PC]
+            elif unit_Pc.upper() == "MMHG":
+                unit_Pc = [float(dados) * 133.322 for dados in PC]
+            elif unit_Pc.upper() == "KGF/CM²":
+                unit_Pc = [float(dados) * 98066.52 for dados in PC]
+            elif unit_Pc.upper() == "KGF/IN²":
+                unit_Pc = [float(dados) * 15200.3 for dados in PC]
+            elif unit_Pc.upper() == "PSI":
+                unit_Pc = [float(dados) * 6894.76 for dados in PC]
+        # CONVERSÕES PARA Pa
+        else:
+            if unit_Pc.upper() == "BAR":
+                unit_Pc = [float(dados) + 14.504 for dados in PC]
+            elif unit_Pc.upper() == "PA":
+                unit_Pc = [float(dados) / 6895 for dados in PC]
+            elif unit_Pc.upper() == "ATM":
+                unit_Pc = [float(dados) * 14.696 for dados in PC]
+            elif unit_Pc.upper() == "TORR":
+                unit_Pc = [float(dados) / 51.715 for dados in PC]
+            elif unit_Pc.upper() == "MMHG":
+                unit_Pc = [float(dados) / 51.715 for dados in PC]
+            elif unit_Pc.upper() == "KGF/CM²":
+                unit_Pc = [float(dados) * 14.22 for dados in PC]
+            elif unit_Pc.upper() == "KGF/IN²":
+                unit_Pc = [float(dados) * 2.205 for dados in PC]
+            elif unit_Pc.upper() == "PSI":
+                unit_Pc = [float(dados) for dados in PC]
+            # CONVERSÕES PARA PSI
+        PC = unit_Pc
+        TC = unit_Tc
 
-        pci = input('INSIRA AS PRESSÕES CRÍTICAS P1 P2 P3... EM PSI: ').split(' ')
-        pci = [float(dado) for dado in pci]
-
-        Tpc = sum([x * y for x, y in zip(yi, tci)])
-        Ppc = sum([x * y for x, y in zip(yi, pci)])
-
-if dg != 0:
-    if dg < 0.75:  # gás seco
-        Ppc = (677 + 15 * dg - 37.5 * dg ** 2)  # Psia
-        Tpc = (168 + 325 * dg - 12.5 * dg ** 2)  # Rankine
-    else:
-        Ppc = (706 - 51.7 * dg - 11.1 * dg ** 2)  # * 6894.76
-        Tpc = (187 + 330 * dg - 71.5 * dg ** 2)  # * 5/9
+        Tpc = sum([x * y for x, y in zip(yi, TC)])
+        Ppc = sum([x * y for x, y in zip(yi, PC)])
 
 
-Ci = input(f'POSSUI COMPONENTE INORGÂNICO NO FLUIDO? S/N:  ').upper()
+Ci = input(f'POSSUI COMPONENTE INORGÂNICO NO FLUIDO? S/N>> ').upper()
 
 if Ci == "S":
-    CO2 = input('TEM CO2? "S/N"?: ').upper()
-    if CO2 == "S":
-        YCO2 = float(input('INFORME A FRAÇÃO MOLAR DE CO2:'))
-    else:
-        YCO2 = 0
-
-    N2 = input('TEM N2? "S/N"?: ').upper()
-    if N2 == "S":
-        YN2 = float(input('INFORME A FRAÇÃO MOLAR DE N2:'))
-    else:
-        YN2 = 0
-
-    H2S = input('TEM H2S? "S/N"?: ').upper()
-    if H2S == "S":
-        YH2S = float(input('INFORME A FRAÇÃO MOLAR DE H2S:'))
-    else:
-        YH2S = 0
+    YCO2 = float(input('INFORME A FRAÇÃO MOLAR DE CO2>> '))
+    YN2 = float(input('INFORME A FRAÇÃO MOLAR DE N2>> '))
+    YH2S = float(input('INFORME A FRAÇÃO MOLAR DE H2S>> '))
 
     Ppc = Ppc + 440 * YCO2 + 600 * YH2S - 170 * YN2
     Tpc = Tpc - 80 * YCO2 + 130 * YH2S - 250 * YN2
-
-# 0.02 0.01 0.85 0.04 0.03 0.03 0.02
-# 547.91 227.49 343.33 549.92 666.06 734.46 765.62
-# 1071 493.1 666.4 706.5 616.4 527.9 550.6
-
 """
-Minha dúvida é em relação aos vários ppc's e tpc's que obtivemos e se eles vão interferir nos cálculos dos componentes
-inorgânicos na mistura
+yi = 0.02 0.01 0.85 0.04 0.03 0.03 0.02
+tc = 547.91 227.49 343.33 549.92 666.06 734.46 765.62
+pc = 1071 493.1 666.4 706.5 616.4 527.9 550.6
+Zc = 0.288
 """
+Tabs = input('INFORME A TEMPERATURA ABSOLUTA (K, C, R OU F) SEGUIDA COM A SIGLA E SEPERADOS POR ESPAÇO>> ').split(' ')
+if Dado == "HY":
+    if Tabs[1].upper() == "C":
+        Tabs = float(Tabs[0]) + 273.15
+    elif Tabs[1].upper() == "R":
+        Tabs = float(Tabs[0]) * 5/9
+    elif Tabs[1].upper() == "F":
+        Tabs = (float(Tabs[0]) - 32) * 5/9 + 273.15
+    elif Tabs[1].upper() == "K":
+        Tabs = float(Tabs[0]) * 1
+# CONVERSÕES PARA K
+else:
+    if Tabs[1].upper() == "C":
+        Tabs = float(Tabs[0]) + 491.67
+    elif Tabs[1].upper() == "R":
+        Tabs = float(Tabs[0])
+    elif Tabs[1].upper() == "F":
+        Tabs = (float(Tabs[0]) + 459.67)
+    elif Tabs[1].upper() == "K":
+        Tabs = float(Tabs[0]) * 1.8
+# CONVERSÕES PARA RANKINE
 
-Tabs = input('INFORME A TEMPERATURA ABSOLUTA (K, C, R OU F) SEGUIDA COM A SIGLA E SEPERADOS POR ESPAÇO:  ').split(' ')
-
-if Tabs[1].upper() == "C":
-    Tabs = float(Tabs[0]) + 491.67
-elif Tabs[1].upper() == "R":
-    Tabs = float(Tabs[0])
-elif Tabs[1].upper() == "F":
-    Tabs = (float(Tabs[0]) + 459.67)   # Precisamos verificar
-elif Tabs[1].upper() == "K":
-    Tabs = float(Tabs[0]) * 1.8
-
-Pabs = input('INFORME A PRESSÃO ABSOLUTA (PA, BAR, PSI) SEGUIDA COM A SIGLA E SEPARADO POR ESPAÇO:  ').upper().split(' ')
-
-if Pabs[1].upper() == "BAR":
-    Pabs = (float(Pabs[0]) * 14.503)
-elif Pabs[1].upper() == "PSI":
-    Pabs = float(Pabs[0])
-elif Pabs[1].upper() == "PA":
-    Pabs = (float(Pabs[0])) * 0.000145
-
+Pabs = input('INFORME A PRESSÃO ABSOLUTA (Bar Pa Atm Torr mmHg Kgf/cm² Kgf/in² ou Psi) SEGUIDA COM A SIGLA E SEPARADO '
+             'POR ESPAÇO>> ').split(' ')
+if Dado == "HY":
+    if Pabs[1] == "BAR":
+        Pabs = float(Pabs[0]) * 100_000
+    elif Pabs[1].upper() == "PA":
+        Pabs = float(Pabs[0])
+    elif Pabs[1].upper() == "ATM":
+        Pabs = float(Pabs[0]) * 101_325
+    elif Pabs[1].upper() == "TORR":
+        Pabs = float(Pabs[0]) * 133.322
+    elif Pabs[1].upper() == "MMHG":
+        Pabs = float(Pabs[0]) * 133.322
+    elif Pabs[1].upper() == "KGF/CM²":
+        Pabs = float(Pabs[0]) * 98066.52
+    elif Pabs[1].upper() == "KGF/IN²":
+        Pabs = float(Pabs[0]) * 15200.3
+    elif Pabs[1].upper() == "PSI":
+        Pabs = float(Pabs[0]) * 6894.76
+    # CONVERSÕES PARA PA
+else:
+    if Pabs[1] == "BAR":
+        Pabs = float(Pabs[0]) + 14.504
+    elif Pabs[1].upper() == "PA":
+        Pabs = float(Pabs[0]) / 6895
+    elif Pabs[1].upper() == "ATM":
+        Pabs = float(Pabs[0]) * 14.696
+    elif Pabs[1].upper() == "TORR":
+        Pabs = float(Pabs[0]) / 51.715
+    elif Pabs[1].upper() == "MMHG":
+        Pabs = float(Pabs[0]) / 51.715
+    elif Pabs[1].upper() == "KGF/CM²":
+        Pabs = float(Pabs[0]) * 14.22
+    elif Pabs[1].upper() == "KGF/IN²":
+        Pabs = float(Pabs[0]) * 2.205
+    elif Pabs[1].upper() == "PSI":
+        Pabs = float(Pabs[0])
+# CONVERSÕES PARA PSI
 
 Ppr = Pabs/Ppc
-Tpr = Tabs/Tpc  # ele tá considerando o Tabs como string? Acho que vou paasr para tipo float para ver o que está havendo
-
-
-Dado = input('DEFINA A CORRELAÇÃO: DIGITE "BB" PARA BRILL E BEGGS, "PP" PARA PAPAY, '
-             '"HY" PARA HALL YARBOROUGH, "DK" PARA DRANCHUK & ABU-KASSEM:  ').upper()
+Tpr = Tabs/Tpc
 
 if Dado == "PP":
     FatorZ = correlacao_Papay(Tpr, Ppr)
-    print(f'FATOR Z PARA CORRELAÇÃO DE PAPAY:  {FatorZ}')
+    print(f'FATOR Z PARA CORRELAÇÃO DE PAPAY>> {FatorZ}')
 
 elif Dado == 'BB':
     if 1.2 < Tpr < 2.4 and 0 < Ppr < 13:
         FatorZ = correlacao_de_Brill_e_Beggs(Ppr, Tpr)
-        print(f'FATOR Z PARA CORRELAÇÃO DE BRILL E BEGGS:  {FatorZ}')
+        print(f'FATOR Z PARA CORRELAÇÃO DE BRILL E BEGGS>> {FatorZ}')
     else:
-        print('TPR OU PPR ESTÁ FORA DO LIMITE!')
+        print('TPR OU PPR ESTÁ FORA DO INTERVALO!!!')
 
 elif Dado == "HY":
     if 1.2 < Tpr < 3 and 0.1 < Ppr < 24:
-        x0 = correlacao_Papay(Ppr, Tpr)
-        FatorZ = correlacao_de_Hall_Yarborough(Ppr, Tpr, x0)
-        print(f'FATOR Z PARA CORRELAÇÃO DE HALL-YARBOROUGH:  {FatorZ}')
+        # x0 = correlacao_Papay(Ppr, Tpr)
+        FatorZ = correlacao_de_Hall_Yarborough(Ppr, Tpr)
+        print(f'FATOR Z PARA CORRELAÇÃO DE HALL-YARBOROUGH>> {FatorZ}')
     else:
-        print('TPR OU PPR ESTÁ FORA DO LIMITE!')
+        print('TPR OU PPR ESTÁ FORA DO INTERVALO!!!')
 
 elif Dado == "DK":
     if 1 < Tpr < 3 and 0.2 < Ppr < 30:
         x0 = correlacao_Papay(Ppr, Tpr)
-        zc = float(input('INFORME O VALOR DE Z CRÍTICO DA MISTURA:  '))
+        zc = float(input('INFORME O VALOR DE Z CRÍTICO DA MISTURA>> '))
         FatorZ = correlacao_dranchukabukassem(Ppr, Tpr, zc, x0)
-        print(f'FATOR Z PARA CORRELAÇÃO DE DRANCHUK & ABU-KASSEM:  {FatorZ}')
+        print(f'FATOR Z PARA CORRELAÇÃO DE DRANCHUK & ABU-KASSEM>> {FatorZ}')
     else:
-        print('TPR OU PPR ESTÁ FORA DO LIMITE!')
+        print('TPR OU PPR ESTÁ FORA DO INTERVALO!!!')
 
 # NO FUTURO ISSO VAI SAI DAQUI
 print(f'Esse é o valor de Ppc: {Ppc}')
 print(f'Esse é o valor de Tpc: {Tpc}')
 print(f'Esse é o valor de Ppr: {Ppr}')
 print(f'Esse é o valor de Tpr: {Tpr}')
+
+# melhoria. Pegar todos os fatores ao mesmo tempo
---
+++ Fator_Z_Correlacoes_Black_Oil.py
@@ -0,0 +1,110 @@
+"""
+Correlação para fator de COMPRESSIBILIDADE ISOTÉRMICO
+Usando newton-raphson
+"""
+from sympy import *
+import math as M
+
+
+def correlacao_de_Brill_e_Beggs(Ppr, Tpr):
+    """
+    :param Ppr: Pressão pseudoreduzida [adimensional]
+    :param Tpr: Temperatura pseudocrítica [adimensional]
+    :return: Fator de compressibilidade do gás
+    """
+    A = 1.39 * (Tpr - 0.92) ** 0.5 - 0.36 * Tpr - 0.101
+    B = (0.62 - 0.23 * Tpr) * Ppr + ((0.066 / (Tpr - 0.86)) - 0.037) * Ppr ** 2 + (
+                0.32 / 10 ** (9 * (Tpr - 1))) * Ppr ** 6
+    C = 0.132 - 0.32 * M.log10(Tpr)
+    D = 10 ** (0.3106 - 0.49 * Tpr + 0.1824 * Tpr ** 2)
+    Z = A + ((1 - A) / M.exp(B)) + C * Ppr ** D
+    return Z
+
+
+def correlacao_Papay(Ppr, Tpr):  # Essa correlação é simples mas tem suas limitações
+    """
+    :param Ppr: Pressão pseudoreduzida [adimensional]
+    :param Tpr: Temperatura pseudocrítica [adimensional]
+    :return: Fator de compressibilidade do gás
+
+    ? <Tpr< ?
+    ? <Ppr< ?
+
+    Desvantagens: ?
+    """
+    Z = 1 - ((3.53 * Ppr) / (10 ** (0.9813 * Tpr))) + ((0.274 * Ppr ** 2) / (10 ** (0.8157 * Tpr)))
+    return Z
+
+
+def correlacao_de_Hall_Yarborough(Ppr, Tpr):
+    """
+    :param Ppr: Pressão pseudoreduzida [adimensional]
+    :param Tpr: Temperatura pseudocrítica [adimensional]
+    :return: Fator de compressibilidade do gás
+
+    Obs.: Usando o método da bissecção
+    """
+    t = 1 / Tpr
+    X1 = 0.06125 * t * M.exp(-1.2 * (1 - t)**2)
+    X2 = 14.76 * t - 9.76 * t**2 + 4.58 * t**3
+    X3 = 90.7 * t - 242.2 * t**2 + 42.4 * t**3
+    X4 = 2.18 + (2.82 * (t))
+
+    # Definindo a função simbólica
+    Y = Symbol('Y')
+    fy = - X1 * Ppr + ((Y + Y**2 + Y**3 - Y**4) / (1 - Y)**3) - X2 * Y**2 + X3 * Y**(X4)
+    derivada = fy.diff(Y)
+
+    # Definindo a derivada numérica
+    fderivada = lambdify(Y, derivada, 'sympy')
+
+    # Transformando a função numericamente
+    fy = lambda Y: - X1 * Ppr + ((Y + Y**2 + Y**3 - Y**4) / (1 - Y)**3) - X2 * Y**2 + X3 * Y**(X4)
+
+    parad = 0.0000001
+    maxit = 500
+    iter = 0
+    xr = 0.1  # 1ºchute
+
+    while True:
+        xold = xr
+        xr = xr - ((fy(xr))/(fderivada(xr)))
+        epest = abs((xold - xr)/xold) * 100
+
+        if epest <= parad or iter >= maxit:
+            break
+    Z = (X1 * Ppr) / xr
+    return Z
+
+
+def correlacao_dranchukabukassem(Ppr, Tpr, zc, x0):
+    """
+    :param Ppr: Pressão pseudoreduzida [adimensional]
+    :param Tpr: Temperatura pseudocrítica [adimensional]
+    :param zc: z crítico. Fornecido pelo usuário?
+    :param x0: Valor que zera função objetivo
+    :return: Fator de compressibilidade do gás
+    """
+    A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11 = 0.3265, -1.0700, -0.5339, 0.01569, -0.05165, 0.5475, -0.7361, \
+                                                   0.1844, 0.1056, 0.6134, 0.7210
+    F = lambda z: 1 + (A1 + A2 / Tpr + A3 / Tpr ** 3 + A4 / Tpr ** 4 + A5 / Tpr ** 5) * ((zc * Ppr) / (z * Tpr)) + (
+                A6 + A7 / Tpr + A8 / Tpr ** 2) * \
+                  ((zc * Ppr) / (z * Tpr)) ** 2 - A9 * (A7 / Tpr + A8 / Tpr ** 2) * ((zc * Ppr) / (z * Tpr)) ** 5 + \
+                  A10 * (1 + A11 * ((zc * Ppr) / (z * Tpr)) ** 2) * (
+                              (((zc * Ppr) / (z * Tpr)) ** 2) * M.exp(-A11 * ((zc * Ppr) / (z * Tpr)) ** 2)) / (
+                              Tpr ** 3) - z
+
+    Pert = 10 ** -6  # pertubation
+    Parad = 10 ** -7  # Stop's Criterion
+    maxit = 500
+    iter = 0
+
+    while True:
+        iter += 1
+        xold = x0
+        x0 = xold - ((Pert * xold * F(xold)) / (F(xold + Pert * xold) - F(x0)))
+        Erro = ((xold - x0) / xold)  # verificar se o erro deve estar em porcentagem ou se é muito grande
+        if Erro <= Parad or iter >= maxit:
+            break
+    z = x0
+    return z
