10730add61c0d75ab7c7c1dd6a76391d1c40933f
Francisco Araújo <franciscoaraujo2016f2@outlook.com>
2022-07-13 00:14:23 -0300

    initial commit

--- Fator_Z_Correlacoes_Black_Oil.py
+++ Fator_Z_Correlacoes_Black_Oil.py
@@ -4,6 +4,7 @@ Usando newton-raphson
 """
 from sympy import *
 import math as M
+import time
 
 
 def correlacao_de_Brill_e_Beggs(Ppr, Tpr):
@@ -12,13 +13,15 @@ def correlacao_de_Brill_e_Beggs(Ppr, Tpr):
     :param Tpr: Temperatura pseudocrítica [adimensional]
     :return: Fator de compressibilidade do gás
     """
+    inicio = time.time()
     A = 1.39 * (Tpr - 0.92) ** 0.5 - 0.36 * Tpr - 0.101
     B = (0.62 - 0.23 * Tpr) * Ppr + ((0.066 / (Tpr - 0.86)) - 0.037) * Ppr ** 2 + (
                 0.32 / 10 ** (9 * (Tpr - 1))) * Ppr ** 6
     C = 0.132 - 0.32 * M.log10(Tpr)
     D = 10 ** (0.3106 - 0.49 * Tpr + 0.1824 * Tpr ** 2)
     Z = A + ((1 - A) / M.exp(B)) + C * Ppr ** D
-    return Z
+    fim = time.time()
+    return f'FATOR Z PARA CORRELAÇÃO DE BRILL E BEGGS>> {Z}, TEMPO>> {fim - inicio}s'
 
 
 def correlacao_Papay(Ppr, Tpr):  # Essa correlação é simples mas tem suas limitações
@@ -41,9 +44,8 @@ def correlacao_de_Hall_Yarborough(Ppr, Tpr):
     :param Ppr: Pressão pseudoreduzida [adimensional]
     :param Tpr: Temperatura pseudocrítica [adimensional]
     :return: Fator de compressibilidade do gás
-
-    Obs.: Usando o método da bissecção
     """
+    inicio = time.time()
     t = 1 / Tpr
     X1 = 0.06125 * t * M.exp(-1.2 * (1 - t)**2)
     X2 = 14.76 * t - 9.76 * t**2 + 4.58 * t**3
@@ -70,11 +72,11 @@ def correlacao_de_Hall_Yarborough(Ppr, Tpr):
         xold = xr
         xr = xr - ((fy(xr))/(fderivada(xr)))
         epest = abs((xold - xr)/xold) * 100
-
         if epest <= parad or iter >= maxit:
             break
     Z = (X1 * Ppr) / xr
-    return Z
+    fim = time.time()
+    return f'FATOR Z PARA CORRELAÇÃO DE HALL-YARBOROUGH>> {Z}, TEMPO>> {fim - inicio}s'
 
 
 def correlacao_dranchukabukassem(Ppr, Tpr, zc, x0):
@@ -85,6 +87,7 @@ def correlacao_dranchukabukassem(Ppr, Tpr, zc, x0):
     :param x0: Valor que zera função objetivo
     :return: Fator de compressibilidade do gás
     """
+    inicio = time.time()
     A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11 = 0.3265, -1.0700, -0.5339, 0.01569, -0.05165, 0.5475, -0.7361, \
                                                    0.1844, 0.1056, 0.6134, 0.7210
     F = lambda z: 1 + (A1 + A2 / Tpr + A3 / Tpr ** 3 + A4 / Tpr ** 4 + A5 / Tpr ** 5) * ((zc * Ppr) / (z * Tpr)) + (
@@ -103,8 +106,9 @@ def correlacao_dranchukabukassem(Ppr, Tpr, zc, x0):
         iter += 1
         xold = x0
         x0 = xold - ((Pert * xold * F(xold)) / (F(xold + Pert * xold) - F(x0)))
-        Erro = ((xold - x0) / xold)  # verificar se o erro deve estar em porcentagem ou se é muito grande
+        Erro = ((xold - x0) / xold) * 100  # verificar se o erro deve estar em porcentagem ou se é muito grande
         if Erro <= Parad or iter >= maxit:
             break
     z = x0
-    return z
+    fim = time.time()
+    return f'FATOR Z PARA CORRELAÇÃO DE DRANCHUK & ABU-KASSEM>> {z}, TEMPO>> {fim - inicio}s'
